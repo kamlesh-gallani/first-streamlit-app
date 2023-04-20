c427a15d6e2b934078a7fb2c3702ea31c288eef0
kamlesh-gallani <kamlesh.gallani@outlook.com>
2023-04-20 15:31:18 -0400

    structured

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,21 +27,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
 
-
+# Section to display fruity vice api response
 st.header("Fruityvice Fruit Advice!")
-
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-
-# Normalize the json data received from API response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display the data on page
-st.dataframe(fruityvice_normalized)
-
+try:
+    fruit_choice = st.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        st.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        # Normalize the json data received from API response
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # Display the data on page
+        st.dataframe(fruityvice_normalized)
+except URLError as e:
+    st.error()
+    
 st.stop()
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
